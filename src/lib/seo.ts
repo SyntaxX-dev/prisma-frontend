93c26b88673be70772efdaebae678626a8d23a5f
Breno-Lima <breno.souza@academico.ufpb.br>
2025-11-28 09:17:41 -0300

    feat: add adsense

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -3,6 +3,9 @@ import type { Metadata } from 'next';
 export const defaultMetadata: Metadata = {
 	title: 'Prisma',
 	description: 'Guias, níveis e vídeos para estudos.',
+	other: {
+		'google-adsense-account': 'ca-pub-7249639965881490',
+	},
 };
 
 
