ae648156f72a45b4637dd9f448b5bad9fce1d4ef
Breno-Lima <breno.souza@academico.ufpb.br>
2025-11-28 09:13:58 -0300

    feat: add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { defaultMetadata } from "@/lib/seo";
 import { LoadingProvider } from "@/contexts/LoadingContext";
@@ -37,6 +38,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
         suppressHydrationWarning={true}
       >
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7249639965881490"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
         <QueryProvider>
           <LoadingProvider>
             <UserStatusProvider>
